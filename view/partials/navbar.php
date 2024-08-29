aea1663b24fa76ce823b59ea01408e25a18d2939
Cristian Castaño <81573985+CristianSk23@users.noreply.github.com>
2024-08-28 22:49:35 -0500

    Rama Actualizada

--- view/partials/navbar.php
+++ view/partials/navbar.php
@@ -1,30 +1,6 @@
 <div class="container-menu-desktop">
 	<!-- Topbar -->
-	<div class="top-bar">
-		<div class="content-topbar flex-sb-m h-full container">
-			<div class="left-top-bar">
-				Free shipping for standard order over $100
-			</div>
 
-			<div class="right-top-bar flex-w h-full">
-				<a href="#" class="flex-c-m trans-04 p-lr-25">
-					Help & FAQs
-				</a>
-
-				<a href="#" class="flex-c-m trans-04 p-lr-25">
-					My Account
-				</a>
-
-				<a href="#" class="flex-c-m trans-04 p-lr-25">
-					EN
-				</a>
-
-				<a href="#" class="flex-c-m trans-04 p-lr-25">
-					USD
-				</a>
-			</div>
-		</div>
-	</div>
 
 	<div class="wrap-menu-desktop">
 		<nav class="limiter-menu-desktop container">
@@ -38,14 +14,11 @@
 			<div class="menu-desktop">
 				<ul class="main-menu">
 					<li class="active-menu">
-						<a href="index.php">Home</a>
-						<ul class="sub-menu">
-							<li><a href="index.php">Homepage</a></li>
-						</ul>
+						<a href="index.php">Inicio</a>
 					</li>
 
 					<li>
-						<a href="product.php">Shop</a>
+						<a href="product.php">Comprar</a>
 					</li>
 
 					<li class="label1" data-label1="hot">
