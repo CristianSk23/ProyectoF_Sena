1e2541840dae02f49b5c14efd1c56f35a82ba817
danielsam707 <78787236+danielsam707@users.noreply.github.com>
2024-09-13 18:36:29 -0500

    Registros de productos
    Registro de productos y stocks

--- view/configuracion/producto/registroProducto.php
+++ view/configuracion/producto/registroProducto.php
@@ -1,74 +1,89 @@
-<div class="mt-5">
-    <h4 class="display-4">Registrar Producto</h4>
-</div>
 
-<form class="row g-3 mt-5" method="post" action="<?php echo getUrl('Configuracion','Producto','postInsert'); ?>" enctype="multipart/form-data">
-
-    <!-- Tipo de Prenda -->
-    <div class="form-group col-md-4 mt-4">
-        <label for="tipo">Tipo de prenda</label>
-        <select class="form-control" name="tipo">
-            <option selected disabled value="">Seleccione...</option>
-            <?php foreach ($tipos as $tipo): ?>
-                <option value="<?= $tipo['tipo_id']; ?>"><?= $tipo['tipo_nombre']; ?></option>
-            <?php endforeach; ?>
-        </select>
+<div class="container">
+    <div class="mt-5">
+        <h4 class="display-4">Registrar Producto</h4>
     </div>
+    <form class="row g-3 mt-5" method="post" action="<?php echo getUrl('Configuracion','Producto','postInsert'); ?>" enctype="multipart/form-data">
 
-    <!-- Nombre del Producto -->
-    <div class="form-group col-md-4 mt-4">
-        <label for="nombre">Nombre del Producto</label>
-        <input type="text" class="form-control" id="nombre" name="nombreProducto" placeholder="Ingrese el nombre del producto" required>
-    </div>
 
-     <!-- Categoría -->
-     <div class="form-group col-md-4 mt-4">
-        <label for="categoria">Categoría</label>
-        <select class="form-control" name="categoria">
-            <option selected disabled value="">Seleccione...</option>
-            <?php foreach ($categorias as $categoria): ?>
-                <option value="<?= $categoria['categoria_id']; ?>"><?= $categoria['categoria_nombre']; ?></option>
-            <?php endforeach; ?>
-        </select>
-    </div>
+        
+        <!-- Tipo de Prenda -->
+        <div class="form-group col-md-4 mt-4">
+            <label for="tipo">Tipo de prenda</label>
+            <select class="form-control" name="tipo">
+                <option selected disabled value="">Seleccione...</option>
+                <?php foreach ($tipos as $tipo): ?>
+                    <option value="<?= $tipo['tipo_id']; ?>"><?= $tipo['tipo_nombre']; ?></option>
+                <?php endforeach; ?>
+            </select>
+        </div>
 
-    <!-- Descripción del Producto -->
-    <div class="form-group col-md-8 mt-4">
-        <label for="descripcionProducto">Descripción del Producto</label>
-        <textarea class="form-control" id="descripcionProducto" name="descripcionProducto" rows="4" placeholder="Ingrese una descripción del producto"></textarea>
-    </div>
+        <!-- Nombre del Producto -->
+        <div class="form-group col-md-4 mt-4">
+            <label for="nombre">Nombre del Producto</label>
+            <input type="text" class="form-control" id="nombre" name="nombreProducto" placeholder="Ingrese el nombre del producto" >
+        </div>
 
-   
+        <!-- Categoría -->
+        <div class="form-group col-md-4 mt-4">
+            <label for="categoria">Categoría</label>
+            <select class="form-control" name="categoria">
+                <option selected disabled value="">Seleccione...</option>
+                <?php foreach ($categorias as $categoria): ?>
+                    <option value="<?= $categoria['categoria_id']; ?>"><?= $categoria['categoria_nombre']; ?></option>
+                <?php endforeach; ?>
+            </select>
+        </div>
 
-    <!-- Género -->
-    <div class="form-group col-md-4 mt-4">
-        <label for="genero">Género</label>
-        <select class="form-control" name="genero">
-            <option selected disabled value="">Seleccione...</option>
-            <?php foreach ($generos as $genero): ?>
-                <option value="<?= $genero['genero_id']; ?>"><?= $genero['genero_nombre']; ?></option>
-            <?php endforeach; ?>
-        </select>
-    </div>
-
-    <!-- Imagen del Producto -->
-    <div class="form-group col-md-5 mt-4">
-        <label for="imagen">Agregar fotos del producto</label>
-        <input type="file" class="form-control" name="stock_img[]" id="imageInput" multiple required>
-    </div>
+        <!-- Descripción del Producto -->
+        <div class="form-group col-md-8 mt-4">
+            <label for="descripcionProducto">Descripción del Producto</label>
+            <textarea class="form-control" id="descripcionProducto" name="descripcionProducto" rows="4" placeholder="Ingrese una descripción del producto"></textarea>
+        </div>
 
     
 
-    <!-- Botón de Registro -->
-    <div class="col-12 mt-4">
-        <button class="btn btn-primary" type="submit">Registrar Producto</button>
-    </div>
+        <!-- Género -->
+        <div class="form-group col-md-4 mt-4">
+            <label for="genero">Género</label>
+            <select class="form-control" name="genero">
+                <option selected disabled value="">Seleccione...</option>
+                <?php foreach ($generos as $genero): ?>
+                    <option value="<?= $genero['genero_id']; ?>"><?= $genero['genero_nombre']; ?></option>
+                <?php endforeach; ?>
+            </select>
+        </div>
 
-    <!-- Botón para Consultar Productos Registrados -->
-    <div class="col-12 mt-2">
-        <a href="<?php echo getUrl('Configuracion', 'Producto', 'consultar'); ?>" class="btn btn-primary">Consultar Productos</a>
-    </div>
-    <div class="col-12 mt-4" >
-    </div>
+        <!-- Imagen del Producto -->
+        <div class="form-group col-md-5 mt-4">
+            <label for="imagen">Agregar fotos del producto</label>
+            <input type="file" class="form-control" name="stock_img[]" id="imageInput" multiple >
+        </div>
 
-</form>
+        <br>
+        <?php if(isset($_SESSION['datosIncorrectos'])): ?>
+            <div class="alert alert-danger alert-dismissible fade show col-md-12" role="alert">
+                <?=$_SESSION['datosIncorrectos'] ?>
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+            <?php
+            unset($_SESSION['datosIncorrectos']);
+        endif;
+        ?>
+        <br>
+
+        <!-- Botón de Registro -->
+        <div class="col-12 mt-4">
+            <button class="btn btn-primary" type="submit">Registrar Producto</button>
+        </div>
+
+        <!-- Botón para Consultar Productos Registrados -->
+        <div class="col-12 mt-2">
+            <a href="<?php echo getUrl('Configuracion', 'Producto', 'consultar'); ?>" class="btn btn-primary">Consultar Productos</a>
+        </div>
+       
+        <div class="col-12 mt-4" >
+        </div>
+
+    </form>
+</div>
--- view/configuracion/stock/modificarStock.php
+++ view/configuracion/stock/modificarStock.php
@@ -1,62 +1,64 @@
-<div class="mt-5">
-    <h4 class="display-4">Editar Stock</h4>
+<div class="container">
+    <div class="mt-5">
+        <h4 class="display-4">Editar Stock</h4>
+    </div>
+
+    <?php foreach($stocks as $stock): ?>
+        <form class="row g-3 mt-5" method="post" action="<?php echo getUrl("Configuracion","Stock","modificacion") ?>" >
+
+            <!-- Nombre de prenda -->
+            <div class="form-group col-md-4">
+                <label for="nombrePrenda">Nombre de prenda</label>
+                <select class="form-control" name="idPrenda">
+                    <option value="<?= $stock['product_id']; ?>"><?= $stock['product_nombre']; ?></option>
+                    <?php foreach ($productos as $producto): ?>
+                        <option value="<?= $producto['product_id']; ?>"><?= $producto['product_nombre']; ?></option>
+                    <?php endforeach; ?>
+                </select>
+                
+            </div>
+
+            
+            <!-- Talla -->
+            <div class="form-group col-md-4">
+                <label for="talla">Talla</label>
+                <select class="form-control" name="talla">
+                    <option value="0"><?= $stock['stock_talla']; ?></option>
+                    <option>S</option>
+                    <option>M</option>
+                    <option>L</option>
+                    <option>XL</option>
+                    <option>XXL</option>
+                    <option>Única</option>
+                </select>
+            </div>
+
+            <!-- Color -->
+            <div class="col-md-4">
+                <label for="color" class="form-label">Color</label>
+                <input type="text" class="form-control" id="color" name="color" value="<?= $stock['stock_color']; ?>">
+                <input type="hidden" name="idStock"  value="<?= $stock['stock_id']; ?>" >
+                
+            </div>
+
+            <!-- Precio -->
+            <div class="col-md-4">
+                <label for="precio" class="form-label">Precio</label>
+                <input type="text" class="form-control" id="precio" name="precio" value="<?= $stock['stock_precio']; ?>" placeholder="Ingrese el precio">
+            </div>
+
+            <!-- Cantidad -->
+            <div class="col-md-4">
+                <label for="cantidad" class="form-label">Cantidad</label>
+                <input type="text" class="form-control" id="cantidad" name="cantidad" value="<?= $stock['stock_cantidad']; ?>">
+            </div>
+
+            <!-- Botones de acción -->
+            <div class="col-12 mt-4">
+                <button class="btn btn-primary" type="submit">Actualizar Stock</button>
+            </div>
+            <div class="col-12 mt-4" >
+            </div>
+        </form>
+    <?php endforeach; ?>
 </div>
-
-<?php foreach($stocks as $stock): ?>
-    <form class="row g-3 mt-5" method="post" action="<?php echo getUrl("Configuracion","Stock","modificacion") ?>" >
-
-        <!-- Nombre de prenda -->
-        <div class="form-group col-md-4">
-            <label for="nombrePrenda">Nombre de prenda</label>
-            <select class="form-control" name="idPrenda">
-                <option value="<?= $stock['product_id']; ?>"><?= $stock['product_nombre']; ?></option>
-                <?php foreach ($productos as $producto): ?>
-                    <option value="<?= $producto['product_id']; ?>"><?= $producto['product_nombre']; ?></option>
-                <?php endforeach; ?>
-            </select>
-            
-        </div>
-
-        
-        <!-- Talla -->
-        <div class="form-group col-md-4">
-            <label for="talla">Talla</label>
-            <select class="form-control" name="talla">
-                <option value="0"><?= $stock['stock_talla']; ?></option>
-                <option>S</option>
-                <option>M</option>
-                <option>L</option>
-                <option>XL</option>
-                <option>XXL</option>
-                <option>Única</option>
-            </select>
-        </div>
-
-        <!-- Color -->
-        <div class="col-md-4">
-            <label for="color" class="form-label">Color</label>
-            <input type="text" class="form-control" id="color" name="color" value="<?= $stock['stock_color']; ?>">
-            <input type="hidden" name="idStock"  value="<?= $stock['stock_id']; ?>" >
-            
-        </div>
-
-        <!-- Precio -->
-        <div class="col-md-4">
-            <label for="precio" class="form-label">Precio</label>
-            <input type="text" class="form-control" id="precio" name="precio" value="<?= $stock['stock_precio']; ?>" placeholder="Ingrese el precio">
-        </div>
-
-        <!-- Cantidad -->
-        <div class="col-md-4">
-            <label for="cantidad" class="form-label">Cantidad</label>
-            <input type="text" class="form-control" id="cantidad" name="cantidad" value="<?= $stock['stock_cantidad']; ?>">
-        </div>
-
-        <!-- Botones de acción -->
-        <div class="col-12 mt-4">
-            <button class="btn btn-primary" type="submit">Actualizar Stock</button>
-        </div>
-        <div class="col-12 mt-4" >
-        </div>
-    </form>
-<?php endforeach; ?>
